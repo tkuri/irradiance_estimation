afed4b1a20b82f4a295e56204cbd590bd517a302
tkuri <kuritateppei@gmail.com>
2020-05-04 21:50:59 -0700

    Modified several comments.

--- models/pix2pix_tm2_full_reg_in2_multi_model.py
+++ models/pix2pix_tm2_full_reg_in2_multi_model.py
@@ -3,7 +3,7 @@ from .base_model import BaseModel
 from . import networks
 from torch.nn import functional as F
 
-class Pix2PixTm2McRegIn2MultiModel(BaseModel):
+class Pix2PixTm2FullIn2MultiModel(BaseModel):
     """ This class implements the pix2pix model, for learning a mapping from input images to output images given paired data.
 
     The model training requires '--dataset_mode aligned' dataset.
@@ -48,9 +48,7 @@ class Pix2PixTm2McRegIn2MultiModel(BaseModel):
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
         self.loss_names = ['G_GAN', 'G_L1', 'G_LTMReg_1', 'G_LTMReg_2', 'D_real', 'D_fake']
         # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
-        # self.visual_names = ['real_A', 'fake_B', 'real_B']
-        self.visual_names = ['real_A', 'fake_B', 'real_B', 'real_C', 'real_C_itp', 'matrix_1_0', 'matrix_1_1', 'matrix_1_2', 'matrix_1_3', 'matrix_2_0', 'matrix_2_1', 'matrix_2_2', 'matrix_2_3']
-        # self.visual_names = ['real_A', 'fake_B', 'real_B', 'real_C']
+        self.visual_names = ['real_A', 'fake_B', 'real_B', 'real_C', 'real_C_itp', 'matrix_1', 'matrix_2', 'ltm_slice00', 'ltm_slice12', 'ltm_slice24']
         # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>
         if self.isTrain:
             # self.model_names = ['G', 'D']
@@ -61,15 +59,13 @@ class Pix2PixTm2McRegIn2MultiModel(BaseModel):
         # define networks (both generator and discriminator)
         self.output_nc = opt.output_nc
         self.light_res = opt.light_res
-        self.intermediate_nc = opt.intermediate_nc
         print('opt.output_nc', opt.output_nc)
         print('light_res', self.light_res)
-        print('intermediate_nc', self.intermediate_nc)
 
-        self.netG = networks.define_G(opt.input_nc + opt.input2_nc, opt.output_nc*self.intermediate_nc, opt.ngf, 'unet_256_lastrelu', opt.norm,
+        self.netG = networks.define_G(opt.input_nc + opt.input2_nc, opt.output_nc, opt.ngf, 'unet_256_lastrelu', opt.norm,
                                       not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
                                       
-        self.netG2 = networks.define_G(opt.input_nc + opt.input2_nc, self.intermediate_nc, opt.ngf, 'unet_256_lastrelu', opt.norm,
+        self.netG2 = networks.define_G(opt.input_nc + opt.input2_nc, 1, opt.ngf, 'unet_256_lastrelu', opt.norm,
                                       not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
 
 
@@ -115,55 +111,54 @@ class Pix2PixTm2McRegIn2MultiModel(BaseModel):
         self.matrix_2_gain = 1.0
 
     def forward(self):
-        # print("test")
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
-        sub_matrix1 = self.netG(self.real_AC) # [1, 3xmc, 256, 256]
-        sub_matrix2 = self.netG2(self.real_AC) # [1, mc, 256, 256]
-        sub_matrix2 = F.interpolate(sub_matrix2, (self.light_res, self.light_res), mode='bilinear', align_corners=False)# [1, mc, ls, ls]
-        
+        sub_matrix1 = self.netG(self.real_AC) # [25, 3, 256, 256]
+        sub_matrix2 = self.netG2(self.real_AC) # [25, 1, 256, 256]
+        sub_matrix2 = F.interpolate(sub_matrix2, (self.light_res, self.light_res), mode='bilinear', align_corners=False)# [25, ls, ls]
+
         self.sub_matrix_1 = sub_matrix1.clone()
         self.sub_matrix_2 = sub_matrix2.clone()
-        
+
         self.matrix_1 = torch.clamp((sub_matrix1*self.matrix_1_gain-0.5)/0.5, min=-1.0, max=1.0)
-        self.matrix_1_0 = self.matrix_1[:, [0, self.intermediate_nc, self.intermediate_nc*2], :, :]
-        self.matrix_1_1 = self.matrix_1[:, [1, 1 + self.intermediate_nc, 1 + self.intermediate_nc*2], :, :]
-        self.matrix_1_2 = self.matrix_1[:, [2, 2 + self.intermediate_nc, 3 + self.intermediate_nc*2], :, :]
-        self.matrix_1_3 = self.matrix_1[:, [3, 3 + self.intermediate_nc, 3 + self.intermediate_nc*2], :, :]
         self.matrix_2 = torch.clamp((F.interpolate(sub_matrix2, (self.real_B.size(-2), self.real_B.size(-1)), mode='nearest')*self.matrix_2_gain-0.5)/0.5, min=-1.0, max=1.0)
-        self.matrix_2_0 = torch.unsqueeze(self.matrix_2[:, 0, :, :], 1)
-        self.matrix_2_1 = torch.unsqueeze(self.matrix_2[:, 1, :, :], 1)
-        self.matrix_2_2 = torch.unsqueeze(self.matrix_2[:, 2, :, :], 1)
-        self.matrix_2_3 = torch.unsqueeze(self.matrix_2[:, 3, :, :], 1)
+        
+        sub_matrix1 = sub_matrix1.view(-1, sub_matrix1.size(1)*sub_matrix1.size(2)*sub_matrix1.size(3), 1) # [25, 3x256x256, 1]
+        sub_matrix2 = sub_matrix2.view(-1, 1, sub_matrix2.size(-2)*sub_matrix2.size(-1)) # [25, 1, lsxls]
+        trans_matrix = torch.matmul(sub_matrix1, sub_matrix2) #[25, 3x256x256, lsxls]
 
-        sub_matrix1 = sub_matrix1.view(-1, sub_matrix1.size(1), sub_matrix1.size(2)*sub_matrix1.size(3)) # [1, 3xmc, 256x256]
-        sub_matrix2 = sub_matrix2.view(-1, sub_matrix2.size(1), sub_matrix2.size(2)*sub_matrix2.size(3)) # [1, mc, lsxls]
-        sub_matrix3 = torch.matmul(sub_matrix2, self.real_C_itp_flat) # [1, mc, 1]
+        ltm = torch.transpose(trans_matrix, 1, 2) #[25, 25, 3x256x256]
+        # print('ltm size:', ltm.size())
+        # print('real_B size:', self.real_B.size()) #[25, 3, 256, 256]
 
-        sub_matrix1 = torch.transpose(sub_matrix1, 1, 2) # [1, 256x256, 3xmc]
-        sm1R = sub_matrix1[:, :, 0:self.intermediate_nc] # [1, 256x256, mc]
-        sm1G = sub_matrix1[:, :, self.intermediate_nc:self.intermediate_nc*2]
-        sm1B = sub_matrix1[:, :, self.intermediate_nc*2:self.intermediate_nc*3]
-        bufR = torch.matmul(sm1R, sub_matrix3) # [1, 256x256, 1]
-        bufG = torch.matmul(sm1G, sub_matrix3)
-        bufB = torch.matmul(sm1B, sub_matrix3)
-        buf = torch.cat([bufR, bufG, bufB], dim=2) # [1, 256x256, 3]
-        buf = torch.transpose(buf, 1, 2) # [1, 3, 256x256]
+        ltm = ltm.reshape(ltm.size(0), ltm.size(1)*self.real_B.size(1), self.real_B.size(2)*self.real_B.size(3)) #[25, 25x3, 256x256]
+        ltm = ltm.reshape(ltm.size(0), ltm.size(1), self.real_B.size(2), self.real_B.size(3)) #[25, 25x3, 256, 256]
+
+        self.ltm_slice00 = torch.clamp((ltm[:, 0:3, :, :] - 0.5) / 0.5, min=-1.0, max=1.0) # [25, 3, 256, 256]
+        self.ltm_slice12 = torch.clamp((ltm[:, 3*12:3*12+3, :, :] - 0.5) / 0.5, min=-1.0, max=1.0) # [25, 3, 256, 256]
+        self.ltm_slice24 = torch.clamp((ltm[:, 3*24:3*24+3, :, :] - 0.5) / 0.5, min=-1.0, max=1.0) # [25, 3, 256, 256]
+
+        tmR = trans_matrix[:, 0:256**2, :] # [25, 256x256, lsxls]
+        tmG = trans_matrix[:, 256**2:(256**2)*2, :]
+        tmB = trans_matrix[:, (256**2)*2:(256**2)*3, :]
+
+        bufR = torch.matmul(tmR, self.real_C_itp_flat) # [25, 256x256, 1]
+        bufG = torch.matmul(tmG, self.real_C_itp_flat)
+        bufB = torch.matmul(tmB, self.real_C_itp_flat)
+
+        buf = torch.cat([bufR, bufG, bufB], dim=2) # [25, 256x256, 3]
+        buf = torch.transpose(buf, 1, 2) # [25, 3, 256x256]
         buf = (buf - 0.5) / 0.5
         buf = torch.clamp(buf, min=-1.0, max=1.0)
-        # print('buf:', buf.size())
-        self.fake_B = buf.view(self.real_B.size()) # [1, 3, 256, 256]
+        self.fake_B = buf.view(self.real_B.size()) # [25, 3, 256, 256]
+
 
     def backward_D(self):
         """Calculate GAN loss for the discriminator"""
         # Fake; stop backprop to the generator by detaching fake_B
-        # fake_AB = torch.cat((self.real_A, self.fake_B), 1)  # we use conditional GANs; we need to feed both input and output to the discriminator
-        # pred_fake = self.netD(fake_AB.detach())
         fake_ACB = torch.cat((self.real_AC, self.fake_B), 1)  # we use conditional GANs; we need to feed both input and output to the discriminator
         pred_fake = self.netD(fake_ACB.detach())
         self.loss_D_fake = self.criterionGAN(pred_fake, False)
         # Real
-        # real_AB = torch.cat((self.real_A, self.real_B), 1)
-        # pred_real = self.netD(real_AB)
         real_ACB = torch.cat((self.real_AC, self.real_B), 1)
         pred_real = self.netD(real_ACB)
         self.loss_D_real = self.criterionGAN(pred_real, True)
@@ -174,14 +169,13 @@ class Pix2PixTm2McRegIn2MultiModel(BaseModel):
     def backward_G(self):
         """Calculate GAN and L1 loss for the generator"""
         # First, G(A) should fake the discriminator
-        # fake_AB = torch.cat((self.real_A, self.fake_B), 1)
-        # pred_fake = self.netD(fake_AB)
         fake_ACB = torch.cat((self.real_AC, self.fake_B), 1)
         pred_fake = self.netD(fake_ACB)
         self.loss_G_GAN = self.criterionGAN(pred_fake, True)
+
         # Second, G(A) = B
         self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_L1
-        
+
         # Third, LTM Regularization
         trans_mean_1 = torch.mean(self.sub_matrix_1, dim=0, keepdim=True) # [1, 75, 256, 256]
         trans_mean_1 = trans_mean_1.expand(self.sub_matrix_1.size(0), trans_mean_1.size(1), trans_mean_1.size(2), trans_mean_1.size(3))  # [25, 75, 256, 256]
@@ -189,7 +183,7 @@ class Pix2PixTm2McRegIn2MultiModel(BaseModel):
         trans_mean_2 = torch.mean(self.sub_matrix_2, dim=0, keepdim=True) # [1, 75, 256, 256]
         trans_mean_2 = trans_mean_2.expand(self.sub_matrix_2.size(0), trans_mean_2.size(1), trans_mean_2.size(2), trans_mean_2.size(3))  # [25, 75, 256, 256]
         self.loss_G_LTMReg_2 = self.criterionL1(self.sub_matrix_2, trans_mean_2) * self.opt.lambda_LTMReg_2
-        
+
         # combine loss and calculate gradients
         self.loss_G = self.loss_G_GAN + self.loss_G_L1 + self.loss_G_LTMReg_1 + self.loss_G_LTMReg_2
         self.loss_G.backward()
