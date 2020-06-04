8f606c45933b2d2328cb715d0c22d9d6c527a819
tkuri <kuritateppei@gmail.com>
2020-06-04 00:40:15 -0700

    Fixed bug.

--- data/cgintrinsic_dataset.py
+++ data/cgintrinsic_dataset.py
@@ -1,3 +1,8 @@
+import os.path
+from data.base_dataset import BaseDataset, get_params, get_transform
+from data.image_folder import make_dataset
+from PIL import Image
+
 # import os.path
 # from data.base_dataset import BaseDataset, get_params, get_transform
 # # from data.image_folder import make_dataset
