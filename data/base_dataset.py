54cd49b9b27f740bc85ac04171a9d2a9296e3817
tkuri <kuritateppei@gmail.com>
2020-06-04 03:01:32 -0700

    Fixed bug.

--- data/base_dataset.py
+++ data/base_dataset.py
@@ -112,7 +112,7 @@ def get_transform(opt, params=None, grayscale=False, method=Image.BICUBIC, conve
             transform_list += [transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
     return transforms.Compose(transform_list)
 
-def normalize():
+def normalize(grayscale=False):
     transform_list = []
     if grayscale:
         transform_list += [transforms.Normalize((0.5,), (0.5,))]
