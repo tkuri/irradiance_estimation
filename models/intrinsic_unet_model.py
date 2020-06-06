decc96b6b69527a20fd0e3e19accecc1ccfe3b94
tkuri <kuritateppei@gmail.com>
2020-06-06 01:13:10 -0700

    Fixed bug.

--- models/intrinsic_unet_model.py
+++ models/intrinsic_unet_model.py
@@ -3,7 +3,7 @@ from .base_model import BaseModel
 from . import networks
 
 
-class IntrinsicPix2PixModel(BaseModel):
+class IntrinsicUnetModel(BaseModel):
     """ This class implements the pix2pix model, for learning a mapping from input images to output images given paired data.
 
     The model training requires '--dataset_mode aligned' dataset.
