4bd87e6d859eb8cdcf590f5c1722f269f3999fc0
tkuri <kuritateppei@gmail.com>
2020-06-09 22:09:30 -0700

    Modified to mask BM result.

--- util/visualizer.py
+++ util/visualizer.py
@@ -33,6 +33,11 @@ def calc_bm_shading(visuals):
     added_vis = OrderedDict(tmp)
     return added_vis
 
+def mask_on_image(src, visuals):
+    mask = util.tensor2im(visuals['mask'])
+    src = src * mask
+    return src
+
 
 def jet_on_image(src, visuals, mode='alpha'):
     jet = cv2.applyColorMap(src, cv2.COLORMAP_JET)
@@ -97,6 +102,8 @@ def save_images(webpage, visuals, image_path, aspect_ratio=1.0, width=256, gain=
             ims, txts, links = [], [], []
             for label, im_data in visuals.items():
                 im = util.tensor2im(im_data, gain=gain, ch=c)
+                if label == 'fake_BM':
+                    im = mask_on_image(im, visuals)
                 if label == 'fake_BM' or label == 'real_BM' or label=='radiantest' or label=='BM_shading':
                     im = jet_on_image(im, visuals, mode='alpha')
                 image_name = '%s_%s_%s.png' % (name, label, str(c).zfill(2))
@@ -109,6 +116,8 @@ def save_images(webpage, visuals, image_path, aspect_ratio=1.0, width=256, gain=
     else:
         for label, im_data in visuals.items():
             im = util.tensor2im(im_data, gain=gain)
+            if label == 'fake_BM':
+                im = mask_on_image(im, visuals)
             if label == 'fake_BM' or label == 'real_BM' or label=='radiantest' or label=='BM_shading':
                 im = jet_on_image(im, visuals, mode='alpha')
             image_name = '%s_%s.png' % (name, label)
@@ -201,6 +210,8 @@ class Visualizer():
                 idx = 0
                 for label, image in visuals.items():
                     image_numpy = util.tensor2im(image)
+                    if label == 'fake_BM':
+                        image_numpy = mask_on_image(image_numpy, visuals)
                     if label == 'fake_BM' or label == 'real_BM' or label=='radiantest' or label=='BM_shading':
                         image_numpy = jet_on_image(image_numpy, visuals, mode='alpha')
                     label_html_row += '<td>%s</td>' % label
@@ -230,6 +241,8 @@ class Visualizer():
                 try:
                     for label, image in visuals.items():
                         image_numpy = util.tensor2im(image)
+                        if label == 'fake_BM':
+                            image_numpy = mask_on_image(image_numpy, visuals)
                         if label == 'fake_BM' or label == 'real_BM' or label=='radiantest' or label=='BM_shading':
                             image_numpy = jet_on_image(image_numpy, visuals, mode='alpha')
                         self.vis.image(image_numpy.transpose([2, 0, 1]), opts=dict(title=label),
@@ -243,6 +256,8 @@ class Visualizer():
             # save images to the disk
             for label, image in visuals.items():
                 image_numpy = util.tensor2im(image)
+                if label == 'fake_BM':
+                    image_numpy = mask_on_image(image_numpy, visuals)
                 if label == 'fake_BM' or label == 'real_BM' or label=='radiantest' or label=='BM_shading':
                     image_numpy = jet_on_image(image_numpy, visuals, mode='alpha')
                 img_path = os.path.join(self.img_dir, 'epoch%.3d_%s.png' % (epoch, label))
