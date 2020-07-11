c3d6261507ac32c373d3f837117382b12e88a5b9
tkuri <kuritateppei@gmail.com>
2020-07-10 20:21:47 -0700

    Fixed bug.

--- models/brightest_cas_tm_resnet_model.py
+++ models/brightest_cas_tm_resnet_model.py
@@ -66,7 +66,7 @@ class BrightestCasTmResnetModel(BaseModel):
         self.light_res = opt.light_res
         # self.netG1 = networks.define_G(opt.input_nc, 3, opt.ngf, 'unet_256_multi', opt.norm,
         #                               not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
-        self.netG1 = networks.define_G(opt.input_nc, self.light_res**2, opt.ngf, 'unet_256_multi', opt.norm,
+        self.netG1 = networks.define_G(opt.input_nc, self.light_res**2, opt.ngf, 'unet_256_latent', opt.norm,
                                       not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
         self.netG2 = networks.define_G(opt.input_nc, 1, opt.ngf, 'resnet_9blocks_multi', opt.norm,
                                         not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
