59a7cd0104c31846a7223466f5612989d1d4a2fc
tkuri <kuritateppei@gmail.com>
2020-07-13 00:14:07 -0700

    Fixed bug.

--- models/brightest_mul_inL_model.py
+++ models/brightest_mul_inL_model.py
@@ -169,9 +169,9 @@ class BrightestMulInLModel(BaseModel):
         result = []        
         for i in range(25):
             res = [idx]
-            res_base = self.eval_bp_base(self.mask, self.gt_BA[i], self.gt_BP[i], self.gt_BC[i], self.input[i])
-            res_sh = self.eval_bp_sh(self.mask, self.gt_BA[i], self.gt_BP[i], self.gt_BC[i], self.gt_SH[i])
-            res_pr = self.eval_bp_pr(self.mask, self.gt_BA[i], self.gt_BP[i], self.gt_BC[i], self.pr_BA[i], None, '')
+            res_base = self.eval_bp_base(self.mask, self.gt_BA[i], None, self.gt_BC[i], self.input[i])
+            res_sh = self.eval_bp_sh(self.mask, self.gt_BA[i], None, self.gt_BC[i], self.gt_SH[i])
+            res_pr = self.eval_bp_pr(self.mask, self.gt_BA[i], None, self.gt_BC[i], self.pr_BA[i], None, '')
 
             res = []
             label = self.eval_label()
