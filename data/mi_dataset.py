54991c829b62191f7e0d178a4c1078b3d0f8d799
tkuri <kuritateppei@gmail.com>
2020-07-20 20:17:33 -0700

    Added calc probe stats.

--- data/mi_dataset.py
+++ data/mi_dataset.py
@@ -6,6 +6,7 @@ import pickle
 import torch
 import torch.nn as nn
 from util import util
+import numpy as np
 
 # def make_dataset(list_dir, max_dataset_size=float("inf"), phase='train'):
 #     file_name = list_dir + "img_batch_{}.p".format(phase)
@@ -39,6 +40,62 @@ def MI_make_dataset(list_dir, phase='train'):
     print('images_list:', dir_list)
     return dir_list
 
+def coordinate_map(height=256, width=256):
+    xx = np.arange(0, width)/(width-1) - 0.5
+    xx = np.tile(xx, (height, 1))
+
+    yy = np.arange(0, height)[::-1]/(height-1) - 0.5
+    yy = yy[np.newaxis, :]
+    yy = np.tile(yy.T, (1, width))
+
+    xy = np.sqrt(xx * xx + yy * yy)
+
+    return xx, yy, xy
+
+
+def create_probe_mask(xx, yy, xy):
+    mask = {}
+
+    first_cond = (xy <= 1/6)
+    second_cond = (xy > 1/6) & (xy <= 1/3)
+    third_cond = (xy > 1/3) & (xy <= 1/2)
+
+    upright = (xx > 0) & (yy > 0)
+    downright = (xx > 0) & (yy < 0)
+    upleft = (xx < 0) & (yy > 0)
+    downleft = (xx < 0) & (yy < 0)
+
+    mask['0'] = first_cond
+    mask['1_0'] = second_cond & upright
+    mask['1_1'] = second_cond & downright
+    mask['1_2'] = second_cond & downleft
+    mask['1_3'] = second_cond & upleft
+
+    mask['2_0'] = second_cond & upright & (yy >= xx)
+    mask['2_1'] = second_cond & upright & (yy < xx)
+    mask['2_2'] = second_cond & downright & (yy >= -xx)
+    mask['2_3'] = second_cond & downright & (yy < -xx)
+    mask['2_4'] = second_cond & downleft & (yy < xx)
+    mask['2_5'] = second_cond & downleft & (yy >= xx)
+    mask['2_6'] = second_cond & upleft & (yy < -xx)
+    mask['2_7'] = second_cond & upleft & (yy >= -xx)
+
+    return mask
+
+def calc_probe_stat(L_np):
+    xx, yy, xy = coordinate_map(L.np.shape[0], L.np.shape[1])
+    mask = create_probe_mask(xx, yy, xy)
+
+    L_stat = []
+    L_stat.append(np.mean(L_np[mask['0']>0.5]))
+
+    for i in range(4):
+    L_stat.append(np.mean(L_np[mask['1_{}'.format(i)]>0.5]))
+
+    for i in range(8):
+    L_stat.append(np.mean(L_np[mask['2_{}'.format(i)]>0.5]))
+
+    return L_stat
 
 class MiDataset(BaseDataset):
     """
@@ -92,7 +149,10 @@ class MiDataset(BaseDataset):
             SH_path = self.dataroot + '/results/' + path + '/dir_{}_mip2_SH.png'.format(i)
             gt_SH.append(Image.open(SH_path).convert('RGB'))
 
-
+        result['L_stat'] = []
+        for i in range(25):
+            result['L_stat'].append(calc_probe_stat(np.asarray(L[i])/256.0))
+            
         # apply the same transform to both A and B
         transform_params = get_params(self.opt, srgb_img[0].size)
         srgb_img_transform = get_transform(self.opt, transform_params, grayscale=False, convert=False)
