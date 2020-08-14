d7cddaaaebdc6a4b0f072d7ab04963f03316d36c
tkuri <kuritateppei@gmail.com>
2020-08-14 01:53:26 -0700

    Debug.

--- models/brightest_mul_tm_cas_model.py
+++ models/brightest_mul_tm_cas_model.py
@@ -97,9 +97,9 @@ class BrightestMulTmCasModel(BaseModel):
             input_nc += 1
 
         if opt.LTM:
-            dim_LTM = self.light_res**2
+            self.dim_LTM = self.light_res**2
             if self.opt.enc_LTM:
-                dim_LTM = 5
+                self.dim_LTM = 5
                 self.enc_LTM = networks.init_net(networks.IlluminationEncoder(self.light_res**2, 64, dim_LTM), opt.init_type, opt.init_gain, self.gpu_ids)
 
             self.netG1 = networks.define_G(input_nc, dim_LTM, opt.ngf, netG1name, opt.norm,
@@ -188,7 +188,8 @@ class BrightestMulTmCasModel(BaseModel):
             self.Lt_stat = self.enc_LTM(self.Lt_stat.squeeze(-1)).unsqueeze(-1)   
             print('Lt_stat.shape after:', self.Lt_stat.shape)
         
-        ltm = ltm.view(-1, self.light_res**2, (ltm.size(-1)*ltm.size(-2)))  # [25, 25, 256x256]
+        # ltm = ltm.view(-1, self.light_res**2, (ltm.size(-1)*ltm.size(-2)))  # [25, 25, 256x256]
+        ltm = ltm.view(-1, self.dim_LTM, (ltm.size(-1)*ltm.size(-2)))  # [25, 25, 256x256]
         ltm = torch.transpose(ltm, 1, 2)  # [25, 256x256, 25]
         ltm = torch.matmul(ltm, self.Lt_stat) # L:[25, 25, 1] -> ltm[25, 256x256, 1]
         ltm = torch.transpose(ltm, 1, 2) # [25, 1, 256x256]
